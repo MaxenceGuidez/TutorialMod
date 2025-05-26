799d41681837ad8d8b3490fe409abf56472e0418
MaxenceGuidez <maxence.guigui@gmail.com>
2025-05-26 22:34:40 +0200

    Add firsts CreativeModeTabs

--- src/main/java/com/maxenceguidez/tutorialmod/TutorialMod.java
+++ src/main/java/com/maxenceguidez/tutorialmod/TutorialMod.java
@@ -1,6 +1,7 @@
 package com.maxenceguidez.tutorialmod;
 
 import com.maxenceguidez.tutorialmod.block.ModBlocks;
+import com.maxenceguidez.tutorialmod.item.ModCreativeModeTabs;
 import com.maxenceguidez.tutorialmod.item.ModItems;
 import com.mojang.logging.LogUtils;
 import net.minecraft.world.item.CreativeModeTabs;
@@ -32,6 +33,8 @@ public class TutorialMod {
         modEventBus.addListener(this::commonSetup);
         // Register ourselves for server and other game events we are interested in
         MinecraftForge.EVENT_BUS.register(this);
+
+        ModCreativeModeTabs.register(modEventBus);
 
         ModItems.register(modEventBus);
         ModBlocks.register(modEventBus);
