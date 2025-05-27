1918779bcef95ae560f0b22b33da7371d135f2e9
MaxenceGuidez <maxence.guigui@gmail.com>
2025-05-27 22:48:07 +0200

    Add ModFoodProperties & Add Kohlrabi

--- src/main/java/com/maxenceguidez/tutorialmod/item/ModCreativeModeTabs.java
+++ src/main/java/com/maxenceguidez/tutorialmod/item/ModCreativeModeTabs.java
@@ -21,6 +21,8 @@ public class ModCreativeModeTabs {
                         output.accept(ModItems.RAW_ALEXANDRITE.get());
 
                         output.accept(ModItems.CHISEL.get());
+
+                        output.accept(ModItems.KOHLRABI.get());
                     }).build());
 
     public static final RegistryObject<CreativeModeTab> ALEXANDRITE_BLOCKS_TAB = CREATIVE_MODE_TABS.register("alexandrite_blocks_tab",
