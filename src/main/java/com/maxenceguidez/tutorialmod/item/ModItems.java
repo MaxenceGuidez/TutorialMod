0337a98eb27a5589554e63a1ea0014a247c47dc6
MaxenceGuidez <maxence.guigui@gmail.com>
2025-05-27 23:03:00 +0200

    Add FuelItem & Add AuroraAshes

--- src/main/java/com/maxenceguidez/tutorialmod/item/ModItems.java
+++ src/main/java/com/maxenceguidez/tutorialmod/item/ModItems.java
@@ -2,6 +2,7 @@ package com.maxenceguidez.tutorialmod.item;
 
 import com.maxenceguidez.tutorialmod.TutorialMod;
 import com.maxenceguidez.tutorialmod.item.custom.ChiselItem;
+import com.maxenceguidez.tutorialmod.item.custom.FuelItem;
 import net.minecraft.world.item.Item;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
@@ -17,6 +18,7 @@ public class ModItems {
     public static final RegistryObject<Item> CHISEL = ITEMS.register("chisel", () -> new ChiselItem(new Item.Properties().durability(32)));
 
     public static final RegistryObject<Item> KOHLRABI = ITEMS.register("kohlrabi", () -> new Item(new Item.Properties().food(ModFoodProperties.KOHLRABI)));
+    public static final RegistryObject<Item> AURORA_ASHES = ITEMS.register("aurora_ashes", () -> new FuelItem(new Item.Properties(), 1200));
 
     public static void register(IEventBus eventBus) {
         ITEMS.register(eventBus);
