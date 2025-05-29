bff1442a20d9830ada69ef95991987d646a9c670
MaxenceGuidez <maxence.guigui@gmail.com>
2025-05-29 12:55:44 +0200

    Add Armor & Add ModArmorMaterials

--- src/main/java/com/maxenceguidez/tutorialmod/item/ModCreativeModeTabs.java
+++ src/main/java/com/maxenceguidez/tutorialmod/item/ModCreativeModeTabs.java
@@ -32,6 +32,11 @@ public class ModCreativeModeTabs {
                         output.accept(ModItems.ALEXANDRITE_HOE.get());
 
                         output.accept(ModItems.ALEXANDRITE_HAMMER.get());
+
+                        output.accept(ModItems.ALEXANDRITE_HELMET.get());
+                        output.accept(ModItems.ALEXANDRITE_CHESTPLATE.get());
+                        output.accept(ModItems.ALEXANDRITE_LEGGINGS.get());
+                        output.accept(ModItems.ALEXANDRITE_BOOTS.get());
                     }).build());
 
     public static final RegistryObject<CreativeModeTab> ALEXANDRITE_BLOCKS_TAB = CREATIVE_MODE_TABS.register("alexandrite_blocks_tab",
