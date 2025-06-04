5908768a1e7937a48900ad20fa6619fddc7b0f2e
MaxenceGuidez <maxence.guigui@gmail.com>
2025-06-04 21:47:18 +0200

    Add new Alexandrite Ore for Nether and End

--- src/main/java/com/maxenceguidez/tutorialmod/datagen/ModBlockStateProvider.java
+++ src/main/java/com/maxenceguidez/tutorialmod/datagen/ModBlockStateProvider.java
@@ -32,6 +32,8 @@ public class ModBlockStateProvider extends BlockStateProvider {
 
         blockWithItem(ModBlocks.ALEXANDRITE_ORE);
         blockWithItem(ModBlocks.ALEXANDRITE_DEEPSLATE_ORE);
+        blockWithItem(ModBlocks.ALEXANDRITE_NETHER_ORE);
+        blockWithItem(ModBlocks.ALEXANDRITE_END_ORE);
 
         blockWithItem(ModBlocks.MAGIC_BLOCK);
 
