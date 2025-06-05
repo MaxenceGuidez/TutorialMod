db2a2008868fdae0368be53febd293c9807e3b04
MaxenceGuidez <maxence.guigui@gmail.com>
2025-06-05 22:34:36 +0200

    Add Triceratops generation

--- src/main/java/com/maxenceguidez/tutorialmod/event/ModEventBusEvents.java
+++ src/main/java/com/maxenceguidez/tutorialmod/event/ModEventBusEvents.java
@@ -4,8 +4,12 @@ import com.maxenceguidez.tutorialmod.TutorialMod;
 import com.maxenceguidez.tutorialmod.entity.ModEntities;
 import com.maxenceguidez.tutorialmod.entity.client.TriceratopsModel;
 import com.maxenceguidez.tutorialmod.entity.custom.TriceratopsEntity;
+import net.minecraft.world.entity.SpawnPlacementTypes;
+import net.minecraft.world.entity.animal.Animal;
+import net.minecraft.world.level.levelgen.Heightmap;
 import net.minecraftforge.client.event.EntityRenderersEvent;
 import net.minecraftforge.event.entity.EntityAttributeCreationEvent;
+import net.minecraftforge.event.entity.SpawnPlacementRegisterEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 
@@ -19,5 +23,10 @@ public class ModEventBusEvents {
     @SubscribeEvent
     public static void registerAttributes(EntityAttributeCreationEvent event) {
         event.put(ModEntities.TRICERATOPS.get(), TriceratopsEntity.createAttributes().build());
+    }
+
+    @SubscribeEvent
+    public static void registerSpawnPlacements(SpawnPlacementRegisterEvent event) {
+        event.register(ModEntities.TRICERATOPS.get(), SpawnPlacementTypes.ON_GROUND, Heightmap.Types.MOTION_BLOCKING_NO_LEAVES, Animal::checkAnimalSpawnRules, SpawnPlacementRegisterEvent.Operation.REPLACE);
     }
 }
